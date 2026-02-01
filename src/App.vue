9f2b2638b8208bfdb358fd471696650ebd6e9018
Arturo <arturcas2@hotmail.com>
2026-02-01 13:45:24 -0600

    add content in app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <div class="container">
+    <h1 class="title">Cotizer of <span>Cryptos</span></h1>
+  </div>
+
+  <div class="container">
+    <form class="form">
+      <div class="field">
+        <label for="crypto">Crypto:</label>
+        <select name="crypto"></select>
+      </div>
+    </form>
+  </div>
+</template>
 
 <style scoped></style>
