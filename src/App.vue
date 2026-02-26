dc7da2196811242d71269b6293578248905ef3aa
Arturo <arturcas2@hotmail.com>
2026-02-25 21:52:32 -0600

    Add select crypto and code css

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,15 @@ const monedas = ref([
   { codigo: "GBP", texto: "Libra Esterlina" },
 ]);
 
+const cryptos = ref([]);
+
 onMounted(() => {
   const URL =
     "https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,SUPPLY,PRICE,MKT_CAP,VOLUME,CHANGE,TOPLIST_RANK&toplist_quote_asset=USD";
   fetch(URL)
     .then((response) => response.json())
-    .then((data) => {
-      console.log(data.Data.LIST);
+    .then(({ Data: { LIST } }) => {
+      cryptos.value = LIST;
     });
 });
 </script>
@@ -24,12 +26,12 @@ onMounted(() => {
     <h1 class="title">Cotizer of <span>Cryptos</span></h1>
   </div>
 
-  <div class="container">
+  <div class="content">
     <form class="form">
       <div class="field">
-        <label for="crypto">Crypto:</label>
-        <select name="crypto">
-          <option value="">--Selecciona--</option>
+        <label for="currency">Currency:</label>
+        <select name="currency">
+          <option value="">--Select--</option>
           <option
             :value="moneda.codigo"
             v-for="moneda in monedas"
@@ -39,6 +41,22 @@ onMounted(() => {
           </option>
         </select>
       </div>
+
+      <div class="field">
+        <label for="crypto">Crypto:</label>
+        <select name="crypto">
+          <option value="">--Select--</option>
+          <option
+            v-for="crypto in cryptos"
+            :value="crypto.SYMBOL"
+            :key="crypto.ID"
+          >
+            {{ crypto.NAME }}
+          </option>
+        </select>
+      </div>
+
+      <input class="btnSub" type="submit" value="Quote" />
     </form>
   </div>
 </template>
